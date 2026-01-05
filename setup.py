de6f1182aba622f3229b05df4b4d27361f823ab8
Bobby Costin <bobby.kostin@gmail.com>
2026-01-05 05:39:11 -0500

    Optimize: Add aggressive compiler flags for both MSVC and GCC/Clang

--- setup.py
+++ setup.py
@@ -4,18 +4,36 @@ import sys
 
 # Platform-specific compiler flags
 if sys.platform == 'win32':
-    extra_compile_args = ['/O2', '/DNDEBUG']
+    extra_compile_args = [
+        '/O2',           # Maximum optimization (speed)
+        '/Oi',           # Generate intrinsic functions
+        '/Ot',           # Favor fast code
+        '/fp:fast',      # Fast floating point (like -ffast-math)
+        '/GL',           # Whole program optimization
+        '/GS-',          # Disable buffer security checks for speed
+        '/DNDEBUG',      # Disable debug assertions
+    ]
+    extra_link_args = ['/LTCG']  # Link-time code generation
 else:
     extra_compile_args = [
-        '-O3',
-        '-ffast-math', '-funroll-loops', '-finline-functions',
-        '-fomit-frame-pointer', '-DNDEBUG',
+        '-O3',                      # Maximum optimization
+        '-ffast-math',              # Fast floating point math
+        '-funroll-loops',           # Unroll loops
+        '-finline-functions',       # Inline functions aggressively
+        '-fomit-frame-pointer',     # Omit frame pointer for speed
+        '-flto',                    # Link-time optimization
+        '-ftree-vectorize',         # Enable auto-vectorization
+        '-fno-signed-zeros',        # Treat signed zero as unsigned (faster)
+        '-fno-trapping-math',       # Assume floating-point ops don't trap
+        '-DNDEBUG',                 # Disable assertions
     ]
+    extra_link_args = ['-flto']    # Link-time optimization
 
 ext = Extension(
     'pkrbot',
     ['pkrbot.pyx'],
     extra_compile_args=extra_compile_args,
+    extra_link_args=extra_link_args,
 )
 
 setup(
