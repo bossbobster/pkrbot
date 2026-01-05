1716f306dceeb6de32d8ffb0f1433aa7278129c1
Bobby Costin <bobby.kostin@gmail.com>
2026-01-05 05:37:56 -0500

    Fix: Use MSVC-compatible compiler flags on Windows

--- setup.py
+++ setup.py
@@ -1,14 +1,21 @@
 from setuptools import setup, Extension
 from Cython.Build import cythonize
+import sys
+
+# Platform-specific compiler flags
+if sys.platform == 'win32':
+    extra_compile_args = ['/O2', '/DNDEBUG']
+else:
+    extra_compile_args = [
+        '-O3',
+        '-ffast-math', '-funroll-loops', '-finline-functions',
+        '-fomit-frame-pointer', '-DNDEBUG',
+    ]
 
 ext = Extension(
     'pkrbot',
     ['pkrbot.pyx'],
-    extra_compile_args=[
-        '-O3',
-        '-ffast-math', '-funroll-loops', '-finline-functions',
-        '-fomit-frame-pointer', '-DNDEBUG',
-    ],
+    extra_compile_args=extra_compile_args,
 )
 
 setup(
